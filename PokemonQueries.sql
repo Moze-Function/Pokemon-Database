ae05c47f59042016b8700ef0341e9238178b8453
MozeCode <smithmikeice@gmail.com>
2026-03-05 23:59:02 +0000

    changed the stats_id
    ... to match the pokemon_id so that it doesn't run into the same issues as before

--- PokemonQueries.sql
+++ PokemonQueries.sql
@@ -43,7 +43,37 @@ ON pokerefs.hidden_ability_id = HiddenAbilities.hidden_ability_id
 INNER JOIN SubAbilities
 ON pokerefs.sub_ability_id = SubAbilities.sub_ability_id;
 
-SELECT pokedex_num, pokemon_name, region_name, type_name, evolves, sub_type_name, hp, attack, defence, special_atk, special_def, speed, height_m, weight_kg, ability, ability_effect, sub_ability, sub_ability_effect, hidden_ability, hidden_ability_effect
+
+
+SELECT pokemon_name, region_name, type_name, sub_type_name, evolves, hp, attack, defence, special_atk, special_def, speed, height_m, weight_kg, ability, ability_effect, sub_ability, sub_ability_effect, hidden_ability, hidden_ability_effect
+FROM PokeRefs INNER JOIN Pokemon
+ON Pokerefs.pokemon_id = Pokemon.pokemon_id
+INNER JOIN PokeTypes
+ON Pokerefs.type_id = PokeTypes.type_id
+INNER JOIN SubPokeTypes
+ON Pokerefs.sub_type_id = SubPokeTypes.sub_type_id
+-- INNER JOIN Moves
+-- ON Pokerefs.move_id = Moves.move_id
+INNER JOIN Regions
+ON Pokerefs.region_id = Regions.region_id
+INNER JOIN Abilities
+ON Pokerefs.ability_id = Abilities.ability_id
+INNER JOIN SubAbilities
+ON Pokerefs.sub_ability_id = SubAbilities.sub_ability_id
+INNER JOIN HiddenAbilities
+ON Pokerefs.hidden_ability_id = HiddenAbilities.hidden_ability_id
+INNER JOIN Stats
+ON Pokerefs.stat_id = Stats.stat_id;
+
+SELECT *
+FROM Abilities 
+WHERE ability = "defiant";
+
+
+
+-- ============================================================ CODE DEMO ====================================================================
+
+SELECT pokedex_num, pokemon_name, region_name, type_name, sub_type_name, evolves, hp, attack, defence, special_atk, special_def, speed, height_m, weight_kg, ability, ability_effect, sub_ability, sub_ability_effect, hidden_ability, hidden_ability_effect
 FROM PokeRefs INNER JOIN Pokemon
 ON Pokerefs.pokemon_id = Pokemon.pokemon_id
 INNER JOIN PokeTypes
@@ -59,8 +89,27 @@ ON Pokerefs.sub_ability_id = SubAbilities.sub_ability_id
 INNER JOIN HiddenAbilities
 ON Pokerefs.hidden_ability_id = HiddenAbilities.hidden_ability_id
 INNER JOIN Stats
-ON Pokerefs.stat_id = Stats.stat_id;
+ON Pokerefs.stat_id = Stats.stat_id
+-- ----------------------------------------- INPUT -------------------------------------------------------------
+WHERE pokemon_name LIKE (datahere);
 
-SELECT *
-FROM Abilities 
-WHERE ability = "defiant";
+-- ===============================================================================================================================
+SELECT pokedex_num, pokemon_name, region_name, type_name, sub_type_name, evolves, hp, attack, defence, special_atk, special_def, speed, height_m, weight_kg, ability, ability_effect, sub_ability, sub_ability_effect, hidden_ability, hidden_ability_effect
+FROM PokeRefs INNER JOIN Pokemon
+ON Pokerefs.pokemon_id = Pokemon.pokemon_id
+INNER JOIN PokeTypes
+ON Pokerefs.type_id = PokeTypes.type_id
+INNER JOIN SubPokeTypes
+ON Pokerefs.sub_type_id = SubPokeTypes.sub_type_id
+INNER JOIN Regions
+ON Pokerefs.region_id = Regions.region_id
+INNER JOIN Abilities
+ON Pokerefs.ability_id = Abilities.ability_id
+INNER JOIN SubAbilities
+ON Pokerefs.sub_ability_id = SubAbilities.sub_ability_id
+INNER JOIN HiddenAbilities
+ON Pokerefs.hidden_ability_id = HiddenAbilities.hidden_ability_id
+INNER JOIN Stats
+ON Pokerefs.stat_id = Stats.stat_id
+-- ----------------------------------------- INPUT -------------------------------------------------------------
+WHERE pokedex_num LIKE (datahere);
